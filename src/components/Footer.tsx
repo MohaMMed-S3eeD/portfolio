fe72a1d53dcd2499fde71037b902f66ef66c65dd
Mohamed Seed <mosaeed7888@gmail.com>
2025-09-06 21:18:29 +0300

    Added new SVG effects to the Footer component to improve visual design, including a blur filter and colored halos. Improved text layout in the footer section.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -70,6 +70,54 @@ const Footer = () => {
       id="footer-section"
       className="relative px-8 mx-auto h-screen flex flex-col justify-center pb-10 items-center "
     >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 800 450"
+        opacity="1"
+        className="absolute top-0 left-0 w-full h-full -z-30 blur-3xl"
+      >
+        <defs>
+          <filter
+            id="bbblurry-filter"
+            x="-100%"
+            y="-100%"
+            width="400%"
+            height="400%"
+            filterUnits="objectBoundingBox"
+            primitiveUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feGaussianBlur
+              stdDeviation="130"
+              x="0%"
+              y="0%"
+              width="100%"
+              height="100%"
+              in="SourceGraphic"
+              edgeMode="none"
+              result="blur"
+            ></feGaussianBlur>
+          </filter>
+        </defs>
+        <g filter="url(#bbblurry-filter)">
+          <ellipse
+            rx="79.5"
+            ry="150"
+            cx="767.6839031221473"
+            cy="40.31921001076856"
+            fill="hsla(135, 100%, 42%, 1.00)"
+          ></ellipse>
+          <ellipse
+            rx="79.5"
+            ry="150"
+            cx="52.57568917330485"
+            cy="408.6973704350272"
+            fill="hsla(119, 100%, 40%, 1.00)"
+          ></ellipse>
+        </g>
+      </svg>
       <h1 className="footer-title flex flex-col justify-center items-center text-[20px] sm:text-[30px] md:text-[50px] lg:text-[70px] text-center font-bold max-w-full sm:max-w-[80%] md:max-w-[60%] lg:max-w-[40%] md:flex-1 px-4">
         Ready to take your idea to the next level?
         <Magnet padding={100} disabled={false} magnetStrength={15}>
